24a23597d662806e308e4819fd55100b17c43fce
mklf <golifang1234@gmail.com>
2018-11-17 15:16:09 +0800

    change dir layout

---
+++ ffi/setup.py
@@ -0,0 +1,18 @@
+# -*- coding:utf-8 -*-
+import setuptools
+
+setuptools.setup(
+    name="bert_tokenizer",
+    version="0.1.2",
+    author="Li Fang",
+    author_email="golifang1234@gmail.com",
+    description="A Tokenizer for Bert model",
+    packages=setuptools.find_packages(),
+    package_data={'':["*.py","*.so","*.dylib"]},
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 2",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
