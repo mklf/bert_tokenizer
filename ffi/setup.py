f1a3b76b8c9b77d8ad6ecb13482ddacbfafe46bf
mklf <golifang1234@gmail.com>
2018-11-17 23:09:26 +0800

    add readme

--- ffi/setup.py
+++ ffi/setup.py
@@ -3,7 +3,7 @@ import setuptools
 
 setuptools.setup(
     name="bert_tokenizer",
-    version="0.1.3",
+    version="0.1.4",
     author="Li Fang",
     author_email="golifang1234@gmail.com",
     description="A Tokenizer for Bert model",
